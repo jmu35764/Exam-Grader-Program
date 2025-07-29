80e8e0ef6a71346dc52dfd111948bc381b2c3a71
jm underwood <jm.underwood@hotmail.com>
2025-07-28 20:21:30 -0400

    Code works. Just need to clean it up.

--- Exam Grader Program.cpp
+++ Exam Grader Program.cpp
@@ -2,36 +2,157 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 #include <fstream>
+using namespace std;
 
-int main()
+const double MAX_SIZE = 20;
+
+void getAnswers(string filename1, string filename2, char correctarr[],  char studentarr[], int x)
 {
-    std::ifstream File1("CorrectAnswers.txt");
-    std::ifstream File2("StudentAnswers.txt");
-            
+    std::ifstream File1(filename1);
+    std::ifstream File2(filename2);
+
     if (!File1 && !File2)
     {
         std::cerr << "Error Opening File!" << std::endl;
-        return 1;
+        //return 1;
     }
 
-    char correct[20];
-    char student[20];
-    int x = 0;
-    int y = 0;
 
-    while (File1 >> correct[x])
+    while (File1 >> correctarr[x])
     {
-        std::cout << correct[x] << std::endl;
+        //std::cout << std::setw(3) << correct[x] << std::endl;
         x++;
     }
 
-    while (File2 >> student[y])
+    x = 0;
+
+    while (File2 >> studentarr[x])
     {
-        std::cout << student[y] << std::endl;
-        y++;
+        //std::cout << std::setw(3) << correctarr[x] << std::setw(3) << studentarr[x] << std::endl;
+        x++;
     }
+
+    x = 0;
+}
+
+void gradeExam(char correctarr[], char studentarr[], int numarr[], char comparearr[][2], double& wrngAnsw, int x)
+{
+    for (x = 0; x <= (MAX_SIZE - 1); x++)
+    {
+        if (correctarr[x] != studentarr[x])
+        {
+            numarr[x] = x + 1;
+            comparearr[x][0] = correctarr[x];
+            comparearr[x][1] = studentarr[x];
+            wrngAnsw++;
+        }
+
+        else
+        {
+            numarr[x] = 0;
+            comparearr[x][0] = 0;
+            comparearr[x][1] = 0;
+        }
+
+    }
+}
+
+
+void writeReport(int numarr[], char comparearr[][2], double missed, int x)
+{
+    double score = ((MAX_SIZE-missed) / MAX_SIZE)*100;
     
+    cout << "Exam Report Details" << endl;
+    cout << "Number of questions missed: " << missed << endl;
+    cout << "Missed questions and correct answers:" << endl;
+    cout << setw(11) << left << "Question" << setw(17) << left << "Correct Answer" << setw(14) << left << "Your Answer" << endl;
+    
+    
+    for (x = 0; x <= (MAX_SIZE - 1); x++)
+    {
+        if (numarr[x] != 0 && comparearr[x][0] != 0 && comparearr[x][1] != 0)
+        {
+            cout << setw(4) << right << numarr[x];
+            cout << setw(14) << right << comparearr[x][0];
+            cout << setw(18) << right << comparearr[x][1] << endl;
+        }
+    }
+
+    cout << "Test Score:  " << score << "%" << endl;
+
+
+    if (score > 70)
+    {
+        cout << "You passed the exam." << endl;
+    }
+    else
+    {
+        cout << "You failed the exam." << endl;
+    }
+}
+
+int main()
+{
+    char correct[20];
+    char student[20];
+    char compare[20][2];
+    //const int MAX_SIZE = 20;
+    int number[20];
+    double wrongAnswers = 0;
+    int x = 0;
+    int y = 0;
+
+    string filename1 = "CorrectAnswers.txt";
+    string filename2 = "StudentAnswers.txt";
+    
+    
+    //First Function
+    getAnswers(filename1, filename2, correct, student, x);
+    gradeExam(correct, student, number, compare, wrongAnswers, x);
+
+    //cout << wrongAnswers << endl;
+
+    writeReport(number, compare, wrongAnswers, x);
+
+
+    /*for (x = 0; x <= (MAX_SIZE - 1); x++)
+    {
+        if (number[x] != 0 && compare[x][0] != 0 && compare[x][1] != 0)
+        {
+            std::cout << std::setw(5) << number[x] << std::setw(5) << compare[x][0] << std::setw(5) << compare[x][1] << std::endl;
+        }
+    }*/
+    //cout << student[5] << endl;
+
+    /*for (x = 0; x <= (MAX_SIZE - 1); x++)
+    {
+        if (correct[x] != student[x])
+        {
+            number[x] = x+1;
+            compare[x][0] = correct[x];
+            compare[x][1] = student[x];
+            wrongAnswers++;
+        }
+
+        else
+        {
+            number[x] = 0;
+            compare[x][0] = 0;
+            compare[x][1] = 0;
+        }
+
+        if (number[x] != 0 && compare[x][0] != 0 && compare[x][1] != 0)
+        {
+            std::cout << std::setw(5) << number[x] <<std::setw(5) << compare[x][0] << std::setw(5) << compare[x][1] << std::endl;
+        }
+
+
+    }*/
+
+    //std::cout << std::endl << currentSize << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
