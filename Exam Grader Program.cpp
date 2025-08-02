174202ca1eb8d6a372bf2225696dbd160a862593
jm underwood <jm.underwood@hotmail.com>
2025-08-02 17:33:16 -0400

    Added comments and cleaned up code

--- Exam Grader Program.cpp
+++ Exam Grader Program.cpp
@@ -11,19 +11,19 @@ const double MAX_SIZE = 20;
 
 void getAnswers(string filename1, string filename2, char correctarr[],  char studentarr[], int x)
 {
+    // Finds the names of the files to open
     std::ifstream File1(filename1);
     std::ifstream File2(filename2);
 
+    // Makes sure files can be opened
     if (!File1 && !File2)
     {
         std::cerr << "Error Opening File!" << std::endl;
-        //return 1;
     }
 
-
+    //Sets each value from the value into a spot in the array
     while (File1 >> correctarr[x])
     {
-        //std::cout << std::setw(3) << correct[x] << std::endl;
         x++;
     }
 
@@ -31,7 +31,6 @@ void getAnswers(string filename1, string filename2, char correctarr[],  char stu
 
     while (File2 >> studentarr[x])
     {
-        //std::cout << std::setw(3) << correctarr[x] << std::setw(3) << studentarr[x] << std::endl;
         x++;
     }
 
@@ -44,9 +43,18 @@ void gradeExam(char correctarr[], char studentarr[], int numarr[], char comparea
     {
         if (correctarr[x] != studentarr[x])
         {
+            //Store the number of the question that is marked wrong
             numarr[x] = x + 1;
+
+            //Sets the value of the first column of correct array to match 
+            // the correlated value of the correct answer
             comparearr[x][0] = correctarr[x];
+
+            //Sets the value of the second column of correct array to match 
+            // the correlated value of the wrong answer
             comparearr[x][1] = studentarr[x];
+
+            // Tracks total number of wrong answers
             wrngAnsw++;
         }
 
@@ -65,12 +73,13 @@ void writeReport(int numarr[], char comparearr[][2], double missed, int x)
 {
     double score = ((MAX_SIZE-missed) / MAX_SIZE)*100;
     
+    // Displays the header
     cout << "Exam Report Details" << endl;
     cout << "Number of questions missed: " << missed << endl;
     cout << "Missed questions and correct answers:" << endl;
     cout << setw(11) << left << "Question" << setw(17) << left << "Correct Answer" << setw(14) << left << "Your Answer" << endl;
     
-    
+    //Displays the array values
     for (x = 0; x <= (MAX_SIZE - 1); x++)
     {
         if (numarr[x] != 0 && comparearr[x][0] != 0 && comparearr[x][1] != 0)
@@ -116,52 +125,5 @@ int main()
     //cout << wrongAnswers << endl;
 
     writeReport(number, compare, wrongAnswers, x);
-
-
-    /*for (x = 0; x <= (MAX_SIZE - 1); x++)
-    {
-        if (number[x] != 0 && compare[x][0] != 0 && compare[x][1] != 0)
-        {
-            std::cout << std::setw(5) << number[x] << std::setw(5) << compare[x][0] << std::setw(5) << compare[x][1] << std::endl;
-        }
-    }*/
-    //cout << student[5] << endl;
-
-    /*for (x = 0; x <= (MAX_SIZE - 1); x++)
-    {
-        if (correct[x] != student[x])
-        {
-            number[x] = x+1;
-            compare[x][0] = correct[x];
-            compare[x][1] = student[x];
-            wrongAnswers++;
-        }
-
-        else
-        {
-            number[x] = 0;
-            compare[x][0] = 0;
-            compare[x][1] = 0;
-        }
-
-        if (number[x] != 0 && compare[x][0] != 0 && compare[x][1] != 0)
-        {
-            std::cout << std::setw(5) << number[x] <<std::setw(5) << compare[x][0] << std::setw(5) << compare[x][1] << std::endl;
-        }
-
-
-    }*/
-
-    //std::cout << std::endl << currentSize << std::endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
